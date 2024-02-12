451582f3121e80db2bc98d9d6d342861b7310fc0
flysquare <umutkonurinso@gmail.com>
2024-02-12 23:06:05 +0300

    a

--- app/Http/Controllers/SlugController.php
+++ app/Http/Controllers/SlugController.php
@@ -8,17 +8,18 @@ use Illuminate\Http\Request;
 
 class SlugController extends Controller
 {
-    public function slug($customerSlug)
+    public function slug(Request $request, $customerSlug)
     {
         $customer = Customers::where('slug', $customerSlug)->firstOrFail();
         $days = [];
         $sum = 0;
+        $month = $request->has('ay') ? $request->ay : $customer->current_month;
         $getCurrentMonthTotalDaysCount = cal_days_in_month(CAL_GREGORIAN, $customer->current_month, date('Y'));
         for ($i = 1; $i <= $getCurrentMonthTotalDaysCount; $i++) {
             $daySlug = $i < 10 ? '0'.$i : $i;
             $todays =  Monthly::where('customer_id', $customer->hashed_id)
-                ->where('created_at','>=', date('Y').'-'.$customer->current_month.'-'.$daySlug.' 00:00:00')
-                ->where('created_at','<=', date('Y').'-'.$customer->current_month.'-'.$daySlug.' 23:59:59')
+                ->where('created_at','>=', date('Y').'-'.$month.'-'.$daySlug.' 00:00:00')
+                ->where('created_at','<=', date('Y').'-'.$month.'-'.$daySlug.' 23:59:59')
                 ->get();
             $days[$i] = [
                 'records' => $todays,
